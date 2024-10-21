aba78281290f1df66bcf2e4796870c6092d88aa7
Vasylina <Vorobey_Alinka@ukr.net>
2024-10-21 20:34:41 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import NavbarWithMenu from "./components/NavbarWithMenu.tsx";
+
+const App = () => {
+  return (
+    <div>
+      {/*<NavbarSimple />*/}
+      <NavbarWithMenu />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/NavbarSimple.tsx
@@ -0,0 +1,36 @@
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import { MenuItem } from "@mui/material";
+
+const NavbarSimple = () => {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <MenuItem>Contacts</MenuItem>
+          <MenuItem>About</MenuItem>
+          <MenuItem>Gallery</MenuItem>
+          <MenuItem>Auth</MenuItem>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
+
+export default NavbarSimple;
